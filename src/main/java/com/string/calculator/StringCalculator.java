9896c033feb9d2054a23785d8e04fbf97b3ed0b3
Prakshi <prakshijain198@gmail.com>
2021-03-15 00:25:54 +0530

    StringCalculator with newLine delimiter

--- src/main/java/com/string/calculator/StringCalculator.java
+++ src/main/java/com/string/calculator/StringCalculator.java
@@ -1,27 +1,17 @@
 package com.string.calculator;
-import java.util.List;
 import java.util.stream.*;
-
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-	
 	public int Add(String s) {
 		if(!s.isEmpty()) {
-			Stream<String> numbers = Arrays.stream(s.split(","));
+			Stream<String> numbers = Arrays.stream(s.split(",|\n"));
 			return numbers.mapToInt(Integer::parseInt).sum();
 		}
 		return 0;
 	}
 	
-	private boolean isEmpty(String input) {
-		return input.isEmpty();
-	}
-	private int stringToInt(String input) {
-		return Integer.parseInt(input);
-	}
 	
 
 }
--- src/main/java/com/string/test/TestCalculator.java
+++ src/main/java/com/string/test/TestCalculator.java
@@ -29,5 +29,10 @@ public class TestCalculator {
 		StringCalculator calculator = new StringCalculator();
 		assertEquals(calculator.Add("1,2,3"),6);
 	}
+	@Test
+	public void newlineDelimiterValueString() {
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(calculator.Add("1\n2,3"),6);
+	}
 
 }
